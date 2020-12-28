e38bfeef9096df689c5eca287a466e290c64a9f0
prashant1682 <prashant009@gmail.com>
2020-12-28 12:44:01 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,20 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// to setup external css is the same as for normal mapping .
-//When putting the classname it should be in camel case like for JS.
-//   <script src="../src/index.js" type="text/JSX"></script> -- update type to JSX since its reading a JSX file
-// check various attributes applicable for a element
+const img = "https://picsum.photos/200";
+
 ReactDOM.render(
   <div>
     <h1 className="heading" contentEditable="true" spellCheck="fasle">
       My Favourite Foods
     </h1>
+    {/* to get imgae as java script const(img) and render in HTML src */}
+    <img src={img} alt="random pic" />
+    <img src={img + "?greyscale"} alt="random pic" />
+    {/* 
     <ul>
       <li>Bacon</li>
       <li>Jamon</li>
       <li>Noodles</li>
-    </ul>
+    </ul> */}
+    <div>
+      <img
+        className="food-img"
+        src="https://thenovicechefblog.com/wp-content/uploads/2012/01/How-to-cook-bacon-in-the-oven-1-sm-720x405.jpg"
+        alt="Bacon"
+      />
+      <img
+        className="food-img"
+        src="https://images-na.ssl-images-amazon.com/images/I/71lNrnbMXsL._SL1200_.jpg"
+        alt="Jamon"
+      />
+      <img
+        className="food-img"
+        src="https://www.loveandoliveoil.com/wp-content/uploads/2015/03/soy-sauce-noodlesH2.jpg"
+        alt="Noodles"
+      />
+    </div>
   </div>,
   document.getElementById("root")
 );

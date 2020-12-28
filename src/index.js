28bb3cedaf547bc4d66f0cc312deba501799002d
prashant1682 <prashant009@gmail.com>
2020-12-28 11:56:27 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// to setup external css is the same as for normal mapping .
+//When putting the classname it should be in camel case like for JS.
+//   <script src="../src/index.js" type="text/JSX"></script> -- update type to JSX since its reading a JSX file
+// check various attributes applicable for a element
 ReactDOM.render(
   <div>
-    <h1>My Favourite Foods</h1>
+    <h1 className="heading" contentEditable="true" spellCheck="fasle">
+      My Favourite Foods
+    </h1>
     <ul>
       <li>Bacon</li>
       <li>Jamon</li>
